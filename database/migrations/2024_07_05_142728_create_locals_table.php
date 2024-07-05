72458d4830d359eb6730df13a2acf8107d0732b6
Tiago <murtinho@TiagoLaptop>
2024-07-05 15:41:31 +0100

    feat(DEV-3): Criar migrations

---
+++ database/migrations/2024_07_05_142728_create_locals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->longText('description')->nullable();
+            $table->string('coordinates', 45);
+            $table->enum('type', ['beach', 'fluvial', 'cascade']);
+            $table->timestamps();
+            $table->foreignId('districts_id')->constrained('districts', 'id');
+            $table->foreignId('regions_id')->constrained('regions', 'id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locals');
+    }
+};
