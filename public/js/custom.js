96c790c1bd7013a65d8caf5079ea056688b5fea4
Tiago <murtinho@TiagoLaptop>
2024-07-15 14:05:42 +0100

    fix(DEV-12):Melhorar performance de codigo js

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,19 +3,15 @@ function confirmDelete(formId, url) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM fully loaded and parsed');
 
     document.querySelectorAll('.custom-file-input').forEach(function(mediaInput) {
         const mediaLabel = mediaInput.nextElementSibling;
 
         if (mediaInput && mediaLabel) {
-            console.log('Media input and label found for:', mediaInput.id);
 
             mediaInput.addEventListener('change', function() {
-                console.log('Change event triggered on media input:', mediaInput.id);
 
                 if (mediaInput.files.length > 0) {
-                    console.log('File selected:', mediaInput.files[0].name);
                     mediaLabel.textContent = mediaInput.files[0].name; // Atualiza o texto com o nome do arquivo
                 } else {
                     const existingImageAlt = document.querySelector('img.img-thumbnail') ? document.querySelector('img.img-thumbnail').alt : 'Escolher arquivo...';
