faff72ac6f0259dde1b7457ab3702b9d789baac5
Tiago <murtinho@TiagoLaptop>
2024-07-15 15:39:17 +0100

    feat(DEV-14): Documentar o código.

--- app/Models/Local.php
+++ app/Models/Local.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-//use http\Exception\InvalidArgumentException;
+// use http\Exception\InvalidArgumentException;
 use InvalidArgumentException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,13 +11,26 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
+/*
+   Esta classe 'Local' representa um model do Laravel que interage com a tabela 'locals' na bd.
+   Ela implementa a interface 'HasMedia' para facilitar a gestão de mídia associada.
+
+   Utiliza traits que oferecem funcionalidades adicionais:
+   - HasFactory: para suporte a factories de testes.
+   - InteractsWithMedia: para manipulação de arquivos de mídia.
+*/
+
 class Local extends Model implements HasMedia
 {
     use HasFactory, InteractsWithMedia;
 
-    protected $table = 'locals';
+    protected $table = 'locals'; // Define o nome da tabela associada ao model.
 
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'id'; // Define a chave primária do model.
+
+    /*
+        Os atributos que podem ser preenchidos em massa ao criar ou atualizar um model.
+    */
 
     protected $fillable = [
         'name',
@@ -28,6 +41,10 @@ class Local extends Model implements HasMedia
         'regions_id',
     ];
 
+    /*
+        Constante que define os tipos de locais permitidos.
+    */
+
     public const LOCALTYPES = [
         'beach',
         'fluvial',
@@ -36,12 +53,12 @@ class Local extends Model implements HasMedia
 
     public function attributes(): BelongsToMany
     {
-        return $this->belongsToMany(Attribute::class, 'locals_has_attributes',  'locals_id', 'attributes_id');
+        return $this->belongsToMany(Attribute::class, 'locals_has_attributes',  'locals_id', 'attributes_id');  // Define a relação muitos para muitos com o model 'Attribute' usando a tabela intermediária 'locals_has_attributes'.
     }
 
     public function district(): BelongsTo
     {
-        return $this->belongsTo(District::class, 'districts_id', 'id');
+        return $this->belongsTo(District::class, 'districts_id', 'id'); // Define a relação de pertencimento com o modelo 'District' usando 'districts_id'.
     }
 
     public function region(): BelongsTo
@@ -55,14 +72,14 @@ class Local extends Model implements HasMedia
     {
         if (!in_array($value, self::LOCALTYPES)) {
             throw new InvalidArgumentException("Invalid type: $value");
-        }
+        }  // Define um setter para o atributo 'type' que lança uma exceção se o valor não for um tipo válido.
 
-        $this->attributes['type'] = $value;
+        $this->attributes['type'] = $value;  // Define o valor do atributo 'type'.
     }
 
     public function getMediaUrl(): ?string
     {
-        $url = $this->getFirstMediaUrl('locals');
-        return $url ? str_replace('http://localhost', 'http://localhost:8000', $url) : null;
+        $url = $this->getFirstMediaUrl('locals'); // Obtém a URL da primeira mídia da coleção 'locals'.
+        return $url ? str_replace('http://localhost', 'http://localhost:8000', $url) : null; // Ajusta o URL, se necessário.
     }
 }
